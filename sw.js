bfe6646f5ba9b0a19fd5d0cca57f872352998b31
Manabu20200701 <corona-pictures1997@hotmail.co.jp>
2026-09-09 10:53:59 +0900

    トップ画像をひるしか単体に修正

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='hirushika-v0.2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/hirushika.png'];
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/hirushika-hero.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
