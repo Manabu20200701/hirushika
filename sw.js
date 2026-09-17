65f55f85a32aab23cbc90462627d823e821b66d0
Manabu20200701 <corona-pictures1997@hotmail.co.jp>
2026-09-17 18:35:18 +0900

    update V0.63

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE='hirushika-v0-62';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/hirushika-hero.png','./assets/hirushika.png'];
+const CACHE='hirushika-v0-63';
+const ASSETS=[
+  './','./index.html','./manifest.webmanifest',
+  './assets/hirushika-hero.png','./assets/hirushika.png',
+  './assets/hirushika-road.png','./assets/hirushika-road-empty.png',
+  './assets/hirushika-detour.png','./assets/hirushika-chew.png',
+  './assets/hirushika-good-lunch.png','./assets/hirushika-memory.png',
+  './assets/hirushika-time.png','./assets/hirushika-achievement.png'
+];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
